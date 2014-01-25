5eb31bbb866d7b37c8401a5fb2bf99b1c2ba66b9
Rastislav Hudak <hudak.rastislav@gmail.com>
2014-01-25 22:17:47 +0100

    ordering seems ok now

--- public/js/controllers/metadataeditor.js
+++ public/js/controllers/metadataeditor.js
@@ -91,10 +91,6 @@ app.controller('MetadataeditorCtrl', function($scope) {
         });
     };
     
-    // returns 
-    // -1 - if we are 
-    // 0 - if we cannot delete and cannot add
-    // 1 - if we cannot add
     $scope.canDelete = function(child){
     	var a = $scope.getContainingArray(this);  
     	var cnt = 0;
@@ -113,8 +109,21 @@ app.controller('MetadataeditorCtrl', function($scope) {
     	var tobesistr = angular.copy(child);    	
     	// get index of the current element in this array
     	var idx = angular.element.inArray(child, arr); // we loaded jQuery before angular so angular.element should equal jQuery
+    	// increment order of the new element (we are appending to the current one)
+    	// and also all the next elements
+    	// but only if the elements are actually ordered
+    	if(child.ordered){
+    		tobesistr.data_order++;
+    		var i;
+        	for (i = idx+1; i < arr.length; ++i) {
+        		// update only elements of the same type
+        		if(arr[i].xmlns == child.xmlns && arr[i].xmlname == child.xmlname){
+        			arr[i].data_order++;
+        		}
+        	}
+    	}    	
     	// insert into array at specified index, angular will sort the rest out
-    	arr.splice(idx, 0, tobesistr);    
+    	arr.splice(idx+1, 0, tobesistr);    
     }
     
     $scope.deleteElement = function(child){    	
@@ -122,7 +131,17 @@ app.controller('MetadataeditorCtrl', function($scope) {
     	var arr = $scope.getContainingArray(this);	    	    	
     	// get index of the current element in this array
     	var idx = angular.element.inArray(child, arr); // we loaded jQuery before angular so angular.element should equal jQuery
-    	// insert into array at specified index, angular will sort the rest out
+    	// decrement data_order of remaining elements
+    	if(child.ordered){
+	    	var i;
+	    	for (i = idx+1; i < arr.length; ++i) {
+	    		// update only elements of the same type
+        		if(arr[i].xmlns == child.xmlns && arr[i].xmlname == child.xmlname){
+        			arr[i].data_order--;
+        		}
+	    	}
+    	}
+    	// delete
     	arr.splice(idx, 1);    
     }
     
@@ -156,25 +175,21 @@ app.controller('MetadataeditorCtrl', function($scope) {
     	var idx = angular.element.inArray(child, arr);
     	
     	// update the data_order property
-    	child.data_order--;
-    	arr[idx-1].data_order++;
+    	if(child.ordered){
+	    	child.data_order--;
+	    	// only if it's the same type (should be always true
+	    	// because we are checking this in canUpElement)
+    		if(arr[idx-1].xmlns == child.xmlns && arr[idx-1].xmlname == child.xmlname){
+    			arr[idx-1].data_order++;
+    		}
+    	}
     	
     	// move to index--
     	if(idx > 0){
     		arr.move(idx, idx-1);
     	}    	
     }
-    
-    $scope.canUpElement = function(child){
-    	// array of elements which we are going to rearrange
-    	var arr = $scope.getContainingArray(this);
-    	// get index of the current element in this array
-    	var idx = angular.element.inArray(child, arr);
 
-    	return idx > 0;
-    }
-    
-    
     $scope.downElement = function(child){
     	// array of elements which we are going to rearrange
     	var arr = $scope.getContainingArray(this);
@@ -182,20 +197,49 @@ app.controller('MetadataeditorCtrl', function($scope) {
     	var idx = angular.element.inArray(child, arr);
     	
     	// update the data_order property
-    	child.data_order++;
-    	arr[idx+1].data_order--;
+    	if(child.ordered){
+	    	child.data_order++;
+	    	// only if it's the same type (should be always true
+	    	// because we are checking this in canDownElement)
+    		if(arr[idx+1].xmlns == child.xmlns && arr[idx+1].xmlname == child.xmlname){
+    			arr[idx+1].data_order--;
+    		}
+    	}
     	
     	// move to index++
     	arr.move(idx, idx+1);    	
     }
     
+    $scope.canUpElement = function(child){
+    	return child.ordered && (child.data_order > 0);
+    }
+
     $scope.canDownElement = function(child){
+    	
+    	if(!child.ordered){ return false; }
+    	
+    	// this array can contain also another type of elements
+    	// but we only order the same type, so find if there is
+    	// an element of the same type with higher data_ordered
+    	var arr = $scope.getContainingArray(this);
+    	
+	    var i;
+	    for (i = 0; i < arr.length; ++i) {
+	        if(arr[i].data_order > child.data_order){
+	        	return true;
+	        }
+	    }
+    	
+	    return false;
+
+    }
+    
+    // just for debug
+    $scope.getIndex = function(child){
     	// array of elements which we are going to rearrange
     	var arr = $scope.getContainingArray(this);
     	// get index of the current element in this array
-    	var idx = angular.element.inArray(child, arr);
-
-    	return idx < (arr.length-1);
+    	return angular.element.inArray(child, arr);
     }
     
     /*
