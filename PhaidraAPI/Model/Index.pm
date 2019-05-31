8c8eb2383ad636d96ffc1c19371a72bd77a0394d
Giulio <giulio.turetta@unipd.it>
2019-05-31 17:27:44 +0200

    project director role

--- PhaidraAPI/Model/Index.pm
+++ PhaidraAPI/Model/Index.pm
@@ -100,6 +100,7 @@ our %uwm_2_mods_roles = (
   "http://phaidra.univie.ac.at/XML/metadata/lom/V1.0/voc_3/1562629" => "calligrapher",
   "http://phaidra.univie.ac.at/XML/metadata/lom/V1.0/voc_3/1562630" => "transcriber",
   "http://phaidra.univie.ac.at/XML/metadata/lom/V1.0/voc_3/1562631" => "editorofcompilation",
+  "http://phaidra.univie.ac.at/XML/metadata/lom/V1.0/voc_3/1561258" => "projectdirector",
 
   # data supplier -> data contributor
   "http://phaidra.univie.ac.at/XML/metadata/lom/V1.0/voc_3/55" => "dtc",
