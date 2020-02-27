114301bbce4b169ba25b2455a7b2805a8733c952
RastislavHudak <hudak.rastislav@gmail.com>
2020-02-27 15:46:23 +0100

    geonames resolve

--- PhaidraAPI/Controller/Resolve.pm
+++ PhaidraAPI/Controller/Resolve.pm
@@ -3,76 +3,79 @@ package PhaidraAPI::Controller::Resolve;
 use strict;
 use warnings;
 use v5.10;
+use Switch;
 use base 'Mojolicious::Controller';
 
 sub resolve {
   my $self = shift;
 
-	my $res = { alerts => [], status => 200 };
+  my $res = { alerts => [], status => 200 };
 
-	my $uri = $self->param('uri');
-	my $nocache = $self->param('nocache');
+  my $uri = $self->param('uri');
+  my $nocache = $self->param('nocache');
 
-	my $cachekey = $uri;
-	my $cacheval = $self->app->chi->get($cachekey);
+  my $cachekey = $uri;
+  my $cacheval = $self->app->chi->get($cachekey);
 
-	my $miss = 1;
-	if($cacheval){
-		$miss = 0;
-		$self->app->log->debug("[cache hit] $cachekey");
-	}
+  my $miss = 1;
+  if($cacheval){
+    $miss = 0;
+    $self->app->log->debug("[cache hit] $cachekey");
+  }
 
-	if($miss || $nocache){
-		$self->app->log->debug("[cache miss] $cachekey");
-		
-		my $res = $self->_resolve($uri);
-		if($res->{status} ne 200){
-			$self->render(json => { alerts => $res->{alerts} }, status => $res->{status});
-			return;
-		}
+  if($miss || $nocache){
+    $self->app->log->debug("[cache miss] $cachekey");
+    
+    my $res = $self->_resolve($uri);
+    if($res->{status} ne 200){
+      $self->render(json => { alerts => $res->{alerts} }, status => $res->{status});
+      return;
+    }
 
-		$cacheval = $res;
+    $cacheval = $res;
 
-	  $self->app->chi->set($cachekey, $cacheval, '1 day');
-	  $cacheval = $self->app->chi->get($cachekey);
-	}
+    $self->app->chi->set($cachekey, $cacheval, '1 day');
+    $cacheval = $self->app->chi->get($cachekey);
+  }
 
   $self->render(json => { $uri => $cacheval, alerts => $res->{alerts} }, status => $res->{status});
 }
 
 sub _resolve {
 
-	my $self = shift;
-	my $uri = shift;
+  my $self = shift;
+  my $uri = shift;
 
-	my $res = { alerts => [], status => 200 };
+  my $res = { alerts => [], status => 200 };
 
-	if($uri =~ /vocab.getty.edu/g){
-		return $self->_resolve_getty($uri);
-	}elsif ($uri =~ /d-nb.info\/gnd/g){
-		return $self->_resolve_gnd($uri);
-	}else{
-		unshift @{$res->{alerts}}, { type => 'danger', msg => 'Unknown resolver' };
-		$res->{status} = 500;
-		return $res;
-	}
+  if($uri =~ /vocab.getty.edu/g){
+    return $self->_resolve_getty($uri);
+  }elsif ($uri =~ /d-nb.info\/gnd/g){
+    return $self->_resolve_gnd($uri);
+  }elsif ($uri =~ /www.geonames.org/g){
+    return $self->_resolve_geonames($uri);
+  }else{
+    unshift @{$res->{alerts}}, { type => 'danger', msg => 'Unknown resolver' };
+    $res->{status} = 500;
+    return $res;
+  }
 
 }
 
 sub _resolve_gnd {
 
-	my $self = shift;
-	my $uri = shift;
+  my $self = shift;
+  my $uri = shift;
 
-	my $res = { alerts => [], status => 200 };
+  my $res = { alerts => [], status => 200 };
 
-	my $url = Mojo::URL->new($uri);
-	my $get = $self->ua->max_redirects(5)->get($url, => {'Accept' => 'application/ld+json'});
-	if (my $getres = $get->success) {
-		for my $h (@{$getres->json}) {
+  my $url = Mojo::URL->new($uri);
+  my $get = $self->ua->max_redirects(5)->get($url, => {'Accept' => 'application/ld+json'});
+  if (my $getres = $get->success) {
+    for my $h (@{$getres->json}) {
       if($h->{'@id'} eq $uri){
         for my $k (keys %{$h}){
-					if($k eq 'http://d-nb.info/standards/elementset/gnd#preferredNameForTheSubjectHeading'){
+          if($k eq 'http://d-nb.info/standards/elementset/gnd#preferredNameForTheSubjectHeading'){
             for my $vn (@{$h->{$k}}){
               push @{$res->{'skos:prefLabel'}}, $vn;
             }
@@ -85,28 +88,28 @@ sub _resolve_gnd {
         }
       }
     }
-	}else{
-		my ($err, $code) = $get->error;
-		$self->app->log->error("[$uri] error resolving uri ".$self->app->dumper($err));
-		unshift @{$res->{alerts}}, { type => 'danger', msg => $self->app->dumper($err) };
-		$res->{status} =  $code ? $code : 500;
-		return $res;
-	}
+  }else{
+    my ($err, $code) = $get->error;
+    $self->app->log->error("[$uri] error resolving uri ".$self->app->dumper($err));
+    unshift @{$res->{alerts}}, { type => 'danger', msg => $self->app->dumper($err) };
+    $res->{status} =  $code ? $code : 500;
+    return $res;
+  }
 
-	return $res;
+  return $res;
 }
 
 sub _resolve_getty {
 
-	my $self = shift;
-	my $uri = shift;
+  my $self = shift;
+  my $uri = shift;
 
-	my $res = { alerts => [], status => 200 };
+  my $res = { alerts => [], status => 200 };
 
-	my $url = Mojo::URL->new($uri);
-	my $get = $self->ua->max_redirects(5)->get($url, => {'Accept' => 'application/ld+json'});
-	if (my $getres = $get->success) {
-		for my $h (@{$getres->json}) {
+  my $url = Mojo::URL->new($uri);
+  my $get = $self->ua->max_redirects(5)->get($url, => {'Accept' => 'application/ld+json'});
+  if (my $getres = $get->success) {
+    for my $h (@{$getres->json}) {
       if($h->{'@id'} eq $uri){
         for my $k (keys %{$h}){
           if($k eq 'http://www.w3.org/2004/02/skos/core#prefLabel'){
@@ -142,17 +145,107 @@ sub _resolve_getty {
           }
         }
         $res->{'schema:GeoCoordinates'} = $spatial;
-			}
+      }
     }
-	}else{
-		my ($err, $code) = $get->error;
-		$self->app->log->error("[$uri] error resolving uri ".$self->app->dumper($err));
-		unshift @{$res->{alerts}}, { type => 'danger', msg => $self->app->dumper($err) };
-		$res->{status} =  $code ? $code : 500;
-		return $res;
-	}
+  }else{
+    my ($err, $code) = $get->error;
+    $self->app->log->error("[$uri] error resolving uri ".$self->app->dumper($err));
+    unshift @{$res->{alerts}}, { type => 'danger', msg => $self->app->dumper($err) };
+    $res->{status} =  $code ? $code : 500;
+    return $res;
+  }
 
-	return $res;
+  return $res;
+}
+
+sub _resolve_geonames {
+
+  my $self = shift;
+  my $uri = shift;
+
+  my $res = { alerts => [], status => 200 };
+
+  unless ($self->config->{apis}) {
+    my $err = "resolve apis are not configured";
+    $self->app->log->error($err);
+    return { alerts => [ { type => 'danger', msg => $err } ], status => 500 };
+  }
+
+  unless ($self->config->{apis}->{geonames}) {
+    my $err = "geonames api is not configured";
+    $self->app->log->error($err);
+    return { alerts => [ { type => 'danger', msg => $err } ], status => 500 };
+  }
+
+  my $id = $uri =~ s/http:\/\/www\.geonames\.org\///r;
+
+  my $url = Mojo::URL->new($self->config->{apis}->{geonames}->{url}."?username=".$self->config->{apis}->{geonames}->{username}."&geonameId=".$id);
+  my $get = $self->ua->max_redirects(5)->get($url);
+  if (my $getres = $get->success) {
+    my $json = $getres->json;
+    push @{$res->{'skos:prefLabel'}}, $json->{toponymName};
+    my $path = "";
+    if ($json->{adminName5}) {
+      $path .= "--".$json->{adminName5};
+    }
+    if ($json->{adminName4}) {
+      $path .= "--".$json->{adminName4};
+    }
+    if ($json->{adminName3}) {
+      $path .= "--".$json->{adminName3};
+    }
+    if ($json->{adminName2}) {
+      $path .= "--".$json->{adminName2};
+    }
+    if ($json->{adminName1}) {
+      $path .= "--".$json->{adminName1};
+    }
+    if ($json->{countryName}) {
+      $path .= "--".$json->{countryName};
+    }
+    if ($json->{continentCode}) {
+      my $continentName = "";
+      switch ($json->{continentCode}) {
+        case "AF" {
+          $continentName = "Africa";
+        }
+        case "AS" {
+          $continentName = "Asia";
+        }
+        case "EU" {
+          $continentName = "Europe";
+        }
+        case "NA" {
+          $continentName = "North america";
+        }
+        case "OC" {
+          $continentName = "Oceania";
+        }
+        case "SA" {
+          $continentName = "South america";
+        }
+        case "AN" {
+          $continentName = "Antarctica";
+        }
+      }
+      $path .= "--".$continentName;
+    }
+    push @{$res->{'rdfs:label'}}, { '@value' => $json->{toponymName}.$path };
+
+    my $spatial;
+    $spatial->{'schema:latitude'} = $json->{'lat'} if $json->{'lat'};
+    $spatial->{'schema:longitude'} = $json->{'lng'} if $json->{'lng'};
+    $res->{'schema:GeoCoordinates'} = $spatial;
+
+  }else{
+    my ($err, $code) = $get->error;
+    $self->app->log->error("[$uri] error resolving uri ".$self->app->dumper($err));
+    unshift @{$res->{alerts}}, { type => 'danger', msg => $self->app->dumper($err) };
+    $res->{status} =  $code ? $code : 500;
+    return $res;
+  }
+
+  return $res;
 }
 
 1;
