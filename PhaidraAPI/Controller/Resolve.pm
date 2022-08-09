5a4f1a7d0649316e5ed0354236e0f726798372c5
RastislavHudak <hudak.rastislav@gmail.com>
2022-08-09 10:20:15 +0200

    remove getty resolver

--- PhaidraAPI/Controller/Resolve.pm
+++ PhaidraAPI/Controller/Resolve.pm
@@ -48,10 +48,7 @@ sub _resolve {
 
   my $res = {alerts => [], status => 200};
 
-  if ($uri =~ /vocab.getty.edu/g) {
-    return $self->_resolve_getty($uri);
-  }
-  elsif ($uri =~ /d-nb.info\/gnd/g) {
+  if ($uri =~ /d-nb.info\/gnd/g) {
     return $self->_resolve_gnd($uri);
   }
   elsif ($uri =~ /www.geonames.org/g) {
@@ -96,67 +93,6 @@ sub _resolve_gnd {
     $self->app->log->error("[$uri] error resolving uri " . $res->code . ": " . $res->message);
     unshift @{$res->{alerts}}, {type => 'danger', msg => $res->code . ": " . $res->message};
     $res->{status} = $res->code ? $res->code : 500;
-    return $res;
-  }
-
-  return $res;
-}
-
-sub _resolve_getty {
-
-  my $self = shift;
-  my $uri  = shift;
-
-  my $res = {alerts => [], status => 200};
-
-  my $url = Mojo::URL->new($uri);
-  my $get = $self->ua->max_redirects(5)->get($url, => {'Accept' => 'application/ld+json'});
-  if (my $getres = $get->success) {
-    for my $h (@{$getres->json}) {
-      if ($h->{'@id'} eq $uri) {
-        for my $k (keys %{$h}) {
-          if ($k eq 'http://www.w3.org/2004/02/skos/core#prefLabel') {
-            for my $vn (@{$h->{$k}}) {
-              push @{$res->{'skos:prefLabel'}}, $vn;
-
-              # second cycle to find the parentString
-              for my $k1 (keys %{$h}) {
-                if ($k1 eq 'http://vocab.getty.edu/ontology#parentString') {
-                  for my $vn1 (@{$h->{$k1}}) {
-                    my $path = $vn1->{'@value'};
-                    $path =~ s/\s//g;
-                    $path =~ s/,/--/g;
-                    push @{$res->{'rdfs:label'}}, {'@value' => $vn->{'@value'} . "--" . $path};
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      if ($h->{'@id'} eq $uri . '-geometry') {
-        my $spatial;
-        for my $k (keys %{$h}) {
-          if ($k eq 'http://schema.org/latitude') {
-            for my $vn (@{$h->{$k}}) {
-              $spatial->{'schema:latitude'} = $vn->{'@value'};
-            }
-          }
-          if ($k eq 'http://schema.org/longitude') {
-            for my $vn (@{$h->{$k}}) {
-              $spatial->{'schema:longitude'} = $vn->{'@value'};
-            }
-          }
-        }
-        $res->{'schema:GeoCoordinates'} = $spatial;
-      }
-    }
-  }
-  else {
-    my ($err, $code) = $get->error;
-    $self->app->log->error("[$uri] error resolving uri " . $self->app->dumper($err));
-    unshift @{$res->{alerts}}, {type => 'danger', msg => $self->app->dumper($err)};
-    $res->{status} = $code ? $code : 500;
     return $res;
   }
 
