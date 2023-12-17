f1a72fcc582c7a28a1d82d263ec032a8e013f49d
rasta <hudak.rastislav@gmail.com>
2023-12-17 12:59:56 +0100

    Update PhaidraAPI.pm

--- PhaidraAPI.pm
+++ PhaidraAPI.pm
@@ -435,7 +435,7 @@ sub startup {
   $r->get('directory/org_get_units')                ->to('directory#org_get_units');
 
   $r->get('search/triples')                         ->to('search#triples');
-  #$r->get'search/select')                           ->to('search#search_solr');
+  # use ?_method=POST if you want to call this from browser
   $r->post('search/select')                         ->to('search#search_solr');
   $r->post('search/get_pids')                       ->to('search#get_pids');
 
