34eb7871b321ed8aa546fda62cd985f39550916c
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2024-03-21 12:35:08 +0100

    isrestricted

--- PhaidraAPI/Model/Oai/Openaire.pm
+++ PhaidraAPI/Model/Oai/Openaire.pm
@@ -1021,13 +1021,16 @@ sub get_metadata_openaire {
   # File Location (MA)
   # oaire:file
   my $mime;
-  my $restricted = 0;
+    my $restricted = 0;
   if (exists($rec->{datastreams})) {
     for my $ds (@{$rec->{datastreams}}) {
       if ($ds eq 'POLICY') {
         $restricted = 1;
       }
     }
+  }
+  if ($rec->{isrestricted}) {
+    $restricted = 1;
   }
   unless ($allRightReserved || $restricted) {
     my @attrs;
