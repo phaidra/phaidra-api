b8ff0895fd4dd1c5c4a9d28ab9cfe0fe82834c88
Rastislav Hudak <rastislav.hudak@univie.ac.at>
2025-03-09 17:05:03 +0100

    process usage stats

--- utils/processUsageStats.sh
+++ utils/processUsageStats.sh
@@ -4,4 +4,4 @@ exec 2>&1
 
 source /home/phaidev/work/perl5/etc/bashrc
 
-perlbrew exec -q perl /usr/local/phaidra/phaidra-api/utils/logStorageSize.pl
+perlbrew exec -q perl /usr/local/phaidra/phaidra-api/utils/processUsageStats.pl
