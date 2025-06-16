ee17a50fdd73894b301371c610e88e1c13630e05
Phaidra Devel (phaidra2) <phaidra.devel@univie.ac.at>
2025-06-16 18:17:34 +0200

    country_code fix

--- utils/processUsageStats.pl
+++ utils/processUsageStats.pl
@@ -78,13 +78,14 @@ $sth->execute();
 while (my $row = $sth->fetchrow_hashref()) {
     my $ip_address = $row->{ip};
     my $numeric_ip = unpack('N', pack('C4', split(/\./, $ip_address)));
-    my $country_code = 'XX';
+    my $country_code = 'xx';
     print "checking $ip_address\n";
     # Find country code matching the IP
     foreach my $range (@ip_ranges) {
         if ($numeric_ip >= $range->{start_ip} && $numeric_ip <= $range->{end_ip}) {
-            $country_code = lc($range->{country_code});
-            $country_code = 'xx' if $range->{country_code} eq 'None';
+            if (length($range->{country_code}) == 2) {
+                $country_code = lc($range->{country_code});
+            }
             print "found country code $country_code\n";
             last;
         }
